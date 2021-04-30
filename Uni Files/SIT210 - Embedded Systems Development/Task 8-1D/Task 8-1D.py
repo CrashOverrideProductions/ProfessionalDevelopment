ccac296642ba83a7549e9c0a1f694062f4c671d2
Justin Bland <46258474+CrashOverrideProductions@users.noreply.github.com>
2021-04-30 19:45:57 +1000

    Update Task 8-1D.py

--- Uni Files/SIT210 - Embedded Systems Development/Task 8-1D/Task 8-1D.py
+++ Uni Files/SIT210 - Embedded Systems Development/Task 8-1D/Task 8-1D.py
@@ -1,18 +1,16 @@
-  
-// =======================================================================
-// Application:      I2C IR Temp 
-// Purpose:          Task 8.1D - RPi I2C 
-// Author:           Justin Bland
-// Date:             12/04/2021
-// Status            In Development 
-// Revisions         12/04/2021 - Create File 
-// =======================================================================
+# =======================================================================
+# Application:      I2C IR Temp 
+# Purpose:          Task 8.1D - RPi I2C 
+# Author:           Justin Bland
+# Date:             12/04/2021
+# Status            In Development 
+# Revisions         12/04/2021 - Create File 
+# =======================================================================
 
-// USEFUL INFORMATION ====================================================
-// - CODE FORMATTING:
-//      Line Length:    75 Characters +~5% if needed 
-//
-// =======================================================================
+# USEFUL INFORMATION ====================================================
+# - CODE FORMATTING:
+#      Line Length:    75 Characters +~5% if needed 
+# =======================================================================
 
 from smbus2 import SMBus
 from mlx90614 import MLX90614
@@ -24,19 +22,19 @@ sensor = MLX90614(bus, address=0x5A)
 loop = True
 
 while loop:
-  irTemp = sensor.get_object_1()
+    irTemp = sensor.get_object_1()
 
-  if irTemp > 40:
-    print "Too Hot", irTemp
+    if irTemp > 40:
+        print("Too Hot", irTemp)
 
-  elif  35 <= irTemp <= 40:
-    print "Just Right", irTemp
+    elif 35 <= irTemp <= 40:
+        print("Just Right", irTemp)
 
-  elif 24 <= irTemp <= 28:
-    print "A Bit Chilly", irTemp
+    elif 24 <= irTemp <= 28:
+        print("A Bit Chilly", irTemp)
 
-  else:
-    print "Too Cold", irTemp
+    else:
+        print("Too Cold", irTemp)
 
-  sleep(1)
+    sleep(1)
 bus.close()
